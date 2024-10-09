0cef2ebd12bbf4729fc128c1b3c4f11710f0d551
ryanherkt3 <ryzys3@gmail.com>
2024-10-10 12:14:31 +1300

    Add page metadata (title & description)

--- app/ladder-predictor/page.tsx
+++ app/ladder-predictor/page.tsx
@@ -1,6 +1,10 @@
-'use client';
-
+import { Metadata } from 'next';
 import DrawFetcher from '../ui/draw-fetcher';
+
+export const metadata: Metadata = {
+    title: 'NRL Ladder Predictor',
+    description: 'Predict the outcome of NRL matches and see how your predictions affect the NRL ladder',
+};
 
 export default function LadderPredictorPage() {
     return <DrawFetcher pageName={'ladder-predictor'} />;
--- app/maxpoints/page.tsx
+++ app/maxpoints/page.tsx
@@ -1,6 +1,10 @@
-'use client';
-
+import { Metadata } from 'next';
 import DrawFetcher from '../ui/draw-fetcher';
+
+export const metadata: Metadata = {
+    title: 'NRL Max Points',
+    description: 'How high or low can each NRL team finish on the ladder',
+};
 
 export default function MaxPointsPage() {
     return <DrawFetcher pageName={'maxpoints'} />;
