8083786818e1691fdac311acb3e86696b8d10b48
ryanherkt3 <ryzys3@gmail.com>
2024-10-12 14:31:00 +1300

    Fix deployment issue

--- app/ui/fixture/score.tsx
+++ app/ui/fixture/score.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
 
-export default function Score({score, winCondition}: {score: number, winCondition: boolean}) {
+export default function Score({score, winCondition}: {score: number | string, winCondition: boolean}) {
     return <div className={clsx('text-3xl w-[45px] text-center', {'font-semibold': winCondition})}>{score}</div>;
 }
