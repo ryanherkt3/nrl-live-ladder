e04a000b67c5dbe081ee85f9614d2cc049cec0a3
ryanherkt3 <ryanherkt@gmail.com>
2025-07-10 23:28:04 +1200

    Fix bye toggle visibility check

--- app/ui/skeletons/skeleton-ladder.tsx
+++ app/ui/skeletons/skeleton-ladder.tsx
@@ -4,11 +4,15 @@ import SkeletonLadderRow from './skeleton-ladder-row';
 import Standings from './../ladder/standings';
 
 export default function SkeletonLadder({ predictorPage }: { predictorPage: boolean; }) {
-    const { TEAMS, FINALS_TEAMS } = NUMS[CURRENTCOMP];
+    const { TEAMS, FINALS_TEAMS, BYES } = NUMS[CURRENTCOMP];
 
     return (
         <div className="px-8 py-6 flex flex-col gap-6">
-            <div className="flex flex-row self-end shimmer w-[200px] h-[72px] xs:h-7"></div>
+            {
+                BYES === 0 || predictorPage ?
+                    null :
+                    <div className="flex flex-row self-end shimmer w-[200px] h-[72px] xs:h-7"></div>
+            }
             <Standings
                 topHalf={getLadderRow(1, FINALS_TEAMS)}
                 bottomHalf={getLadderRow(FINALS_TEAMS + 1, TEAMS)}
